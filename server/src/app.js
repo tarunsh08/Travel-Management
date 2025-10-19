30a8ed8f6c2479691e37a7c6690c4fd37834dc0b
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-10-19 18:09:44 +0530

    update client url

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://travel-management-mu.vercel.app",
     credentials: true,
 }));
 app.use(cookieParser());
